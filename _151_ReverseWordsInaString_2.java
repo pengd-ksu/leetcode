fa845128bc226feacd5db5901dc673dc44d8311e
pengd-ksu <pengd@ksu.edu>
2023-03-13 14:54:48 -0400

    151 reverse words in a string in java;

---
+++ _151_ReverseWordsInaString_2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.trim().split(" ", -1);
+        StringBuilder res = new StringBuilder();
+        for (int i = words.length-1; i >= 0; i--) {
+            if (!words[i].isEmpty()) {
+                res.append(words[i]).append(" ");
+            }
+        }
+        return res.toString().trim();
+    }
+}
