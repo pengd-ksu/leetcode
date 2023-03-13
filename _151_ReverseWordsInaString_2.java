7e5526e4123e6cba38ff8b00219e50b0dc7f4fe9
pengd-ksu <pengd@ksu.edu>
2023-03-13 14:58:14 -0400

    added a comment for 151;

--- _151_ReverseWordsInaString_2.java
+++ _151_ReverseWordsInaString_2.java
@@ -3,6 +3,7 @@ class Solution {
         String[] words = s.trim().split(" ", -1);
         StringBuilder res = new StringBuilder();
         for (int i = words.length-1; i >= 0; i--) {
+            // split usually brings " "
             if (!words[i].isEmpty()) {
                 res.append(words[i]).append(" ");
             }
