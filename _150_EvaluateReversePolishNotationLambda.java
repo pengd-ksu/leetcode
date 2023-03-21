f9b680f667f0cdaffb5a85d84b6a28dcc6e8a721
pengd-ksu <pengd@ksu.edu>
2023-03-21 06:52:07 -0400

    update 150;

---
+++ _150_EvaluateReversePolishNotationLambda.java
@@ -0,0 +1,36 @@
+class Solution {
+    // This solution is from the editorial
+    
+    private static final Map<String, BiFunction<Integer, Integer, Integer>> OPERATIONS = new HashMap<>();
+    
+    // Ensure this only gets done once for ALL test cases.
+    static {
+        OPERATIONS.put("+", (a, b) -> a + b);
+        OPERATIONS.put("-", (a, b) -> a - b);
+        OPERATIONS.put("*", (a, b) -> a * b);
+        OPERATIONS.put("/", (a, b) -> a / b);
+    }
+    
+    public int evalRPN(String[] tokens) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            
+            if (!OPERATIONS.containsKey(token)) {
+                stack.push(Integer.valueOf(token));
+                continue;
+            }
+            
+            int number2 = stack.pop();
+            int number1 = stack.pop();
+            BiFunction<Integer, Integer, Integer> operation;
+            operation = OPERATIONS.get(token);
+            int result = operation.apply(number1, number2);
+            stack.push(result);
+        }
+        
+        return stack.pop();
+        
+    }
+}
